044e367c17faf667b391421bf716a6c62a14de82
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2023-07-08 14:51:51 +0500

    Changes in shop/views.py

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Homepage</h1>')
+    return HttpResponse("<h1>Home page</h1>")
