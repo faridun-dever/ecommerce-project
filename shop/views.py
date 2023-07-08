bbc0f577ba615443d2b64b2fdef7e99e43e2dadc
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2023-07-08 16:02:31 +0500

    Added .html files to project

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Home page</h1>")
+    return render(request, "shop/index.html")
+
+
+def about(request):
+    return render(request, "shop/about.html")
